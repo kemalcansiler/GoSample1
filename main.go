45a0dee023043f960e303b856142d88097c1b7e6
kemalcansiler <kemalcansiler@github.com>
2018-11-26 16:46:16 +0300

    deneme

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	plantCapacities := []float64{30, 30, 30, 60, 100}
+	activePlants := []int{0, 1}
+
+	var gridLoad = 75.
+
+	/*fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Println("Please choose an option: ")
+
+	var option string
+	fmt.Scanln(&option)*/
+	//fmt.Println(option)
+	if option, err := requestOption(); err == nil {
+		fmt.Println("")
+
+		switch option {
+		case "1":
+			generatePlantCapacityReprot(plantCapacities...)
+		case "2":
+			generatePowerGridReport(activePlants, plantCapacities, gridLoad)
+		default:
+			fmt.Println("sadsa")
+		}
+	} else {
+		fmt.Println(err.Error())
+	}
+}
+
+func requestOption() (option string, err error) {
+	fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Println("Please choose an option: ")
+
+	fmt.Scanln(&option)
+
+	if option != "1" && option != "2" {
+		err = errors.New("Invalid option selected")
+	}
+	return
+}
+
+func generatePlantCapacityReprot(plantCapacities ...float64) {
+	for idx, cap := range plantCapacities {
+		fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+	}
+}
+
+func generatePowerGridReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
+	capacity := 0.
+	for _, plantId := range activePlants {
+		capacity += plantCapacities[plantId]
+	}
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+}
