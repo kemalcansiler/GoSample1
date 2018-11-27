7bde423440a87fc6aa706e60677c6168acd626ff
kemalcansiler <kemalcansiler@github.com>
2018-11-27 14:36:22 +0300

    asd

---
+++ gin-gonic/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.Run(":1453")
+}
