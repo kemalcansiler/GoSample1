f64bdb25784e6af6d463f37d081b056cc82a671d
kemalcansiler <kemalcansiler@github.com>
2018-11-27 15:05:24 +0300

    das

---
+++ gin-gonic/src/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+
+	admin := r.Group("/admin")
+	admin.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "admin-overview.html", nil)
+	})
+
+	r.Static("/public", "./public")
+
+	r.Run(":1455")
+}
